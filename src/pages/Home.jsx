b39d428dd132952b67ed4727980093e6847e01d6
Rohan Dongre <rohancollege07@gmail.com>
2023-07-25 16:41:31 +0530

    Image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import Footer from "../components/Footer"
+import Footer from '../components/Footer'
 
-const Home = () =>{
-    return(
-        <>
-        <h1>Home</h1>
-       
-        </>
-    )
+const Home = () => {
+  return (
+    <>
+      <h1>Homse</h1>
+      <img src="./src/data/Images/growth.webp" alt="" />
+    </>
+  )
 }
 
 export default Home
